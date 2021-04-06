3476b8b7a19a50eb9b4afe47679fb8be8204045e
JustinGalante1 <jn2wf@virginia.edu>
2021-04-06 16:20:44 -0400

    improved querying and updated json

--- questions.js
+++ questions.js
@@ -6,6 +6,26 @@ function getCompanyQuestions(company){
             console.log("File read failed:", err);
             return;
         }
-        console.log('File data:', jsonString);
+        try{
+            const companyObject = JSON.parse(json)
+            const problems = companyObject.problems;
+
+            problems.sort(function(a, b){
+                return b.freq - a.freq;
+            });
+
+            console.log("Showing questions for "+companyObject.company+"\n");
+            for(var x=0;x<problems.length;x++){
+                console.log(x+1+ ": " + problems[x].name + ` (${problems[x].freq} times)`);
+                console.log(problems[x].problemStatement);
+                console.log("Solution: "+problems[x].solution);
+                console.log("\n");
+            }
+        }
+        catch(err){
+            console.log("Error: "+err);
+        }
     })
 }
+
+getCompanyQuestions("Bloomberg");
